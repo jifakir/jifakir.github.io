909065142c7d39982a4ed6e61e10ee090a721b38
Jahidul Islam Fakir <jifakir@outlook.com>
2020-10-26 00:13:52 +0800

    Used data from external source in home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebookF, FaTwitter, FaGithub } from 'react-icons/fa';
+import { home } from '../asstes/data';
 import './Home.scss';
 
 export default () => {
@@ -7,8 +8,8 @@ export default () => {
     return (
         <div className="Home">
             <div className="home-wrapper">
-                <h1 className="title">Hi, I am <span className="designing-name">JI Fakir</span></h1>
-                <p className="sub-title">I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
+                <h1 className="title">Hi, I am <span className="designing-name">{home.name}</span></h1>
+    <p className="sub-title">{home.bio}</p>
                 <div className="icon-wrapper">
                     <div className="icon">
                         <a href='http://www.facebook.com' >
---
+++ src/asstes/data.js
@@ -0,0 +1,31 @@
+
+export const home = {
+    name: 'JI Fakir',
+    bio: 'I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more and more interactive with web animations.'
+};
+export const about = {
+    aboutMe: {
+        name: home.name,
+        bio: home.bio,
+        fullName: 'Jahidul Islam Fakir',
+        age: '22',
+        nationality: 'Bangladeshi',
+        language: 'Bengali, English',
+        address: 'Rajoir, Rayenda, Sarankhola, Bagerhat-9330',
+        freelance: 'available'
+    },
+    services: {
+        item1: {
+            title: 'Web Design',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+        },
+        item2: {
+            title: 'Web Development',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+        },
+        item3: {
+            title: 'Mobile Application',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'
+        },
+    }
+};
