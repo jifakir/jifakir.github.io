4b1b56367b61a1d92bcb3dc3fc2d4f3e3417f1bf
Jahidul Islam Fakir <jifakir@outlook.com>
2021-04-01 11:37:51 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e543fdabd72e5cd54299de19d9e0d138.js"
+  "/precache-manifest.ac60910b01f655e84c65145aabd2f686.js"
 );
 
 self.addEventListener('message', (event) => {
